8a3ba7a09afecbf4560af7f29b47ee506f469e37
mmorandini <emmemorandini@gmail.com>
2017-06-18 12:52:15 +0100

    improved the homepage

--- src/js/controllers/home.controller.js
+++ src/js/controllers/home.controller.js
@@ -8,4 +8,9 @@ function HomeCtrl($rootScope){
 
 	$rootScope.$broadcast('hideHeader');
 
+	$('#home-animated-logo').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', () => {
+		$('#start-btn').css('display', 'block');
+		$('#start-btn').addClass('animated fadeIn');
+	});
+
 }
